3316ffc02913a1d7c39a515d980e482727bf1e99
Miqueias Matias <miqueiasmcaetano@gmail.com>
2024-02-02 20:23:55 -0300

    ok

--- src/app/jointgenesis/page.tsx
+++ src/app/jointgenesis/page.tsx
@@ -1,15 +1,26 @@
 import { Metadata } from 'next';
-import ContentPageFirst from '../../components/ContentPageFirst'
+import ContentPageSecond from '../../components/ContentPageSecond'
 
 const titleProduct = 'Joint Genesis Official';
-const linkProduct = 'https://bit.ly/3tEUhYa';
 
 export default function Home() {
+  const title = 'Claim Your Discounted Joint Genesis Below While Stock Lasts'
+  const primaryBgColor = '#049484'
+  const bgColor = '#f4f4fc'
+  const primaryColor = '#fff'
+  const linkProduct = 'https://hop.clickbank.net/?affiliate=miqueias91&vendor=jointgen&v=bvsl&tid=google';
+  const pathImg = '/img/jointgenesis/img.png';
+
   return (
     <>
-        <ContentPageFirst 
-            titleProduct={titleProduct}
+        <ContentPageSecond 
+            titleProduct={titleProduct} 
+            title={title} 
+            primaryBgColor={primaryBgColor} 
+            bgColor={bgColor} 
+            primaryColor={primaryColor} 
             linkProduct={linkProduct} 
+            pathImg={pathImg} 
         />
     </>
   )
